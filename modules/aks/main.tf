992240793daa973cd4ff1f0d0187dc0d49852320
akumar4k <136370161+akumar4k@users.noreply.github.com>
2025-02-01 17:05:32 +0530

    Update main.tf

---
+++ modules/aks/main.tf
@@ -0,0 +1,35 @@
+resource "azurerm_kubernetes_cluster" "dev_aks" {
+  name                = var.aks_dev_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = "devaks"
+
+  default_node_pool {
+    name       = "agentpool"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "prod_aks" {
+  name                = var.aks_prod_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = "prodaks"
+
+  default_node_pool {
+    name       = "agentpool"
+    node_count = 3
+    vm_size    = "Standard_DS3_v2"
+  }
+
+  api_server_authorized_ip_ranges = []  # Private AKS (No Public Access)
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
