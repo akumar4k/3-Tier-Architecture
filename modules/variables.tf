e9e23faf6b4ed4b5122e1726f80b0b6f4ec76c06
akumar4k <akumar4k@gmail.com>
2025-02-01 19:02:06 +0530

    updated kv

---
+++ modules/variables.tf
@@ -0,0 +1,13 @@
+variable "location" {
+  description = "Azure Region"
+  type        = string
+  default     = "East US"
+}
+
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  type        = string
+  default     = "3tier-architecture"
+}
+
+
